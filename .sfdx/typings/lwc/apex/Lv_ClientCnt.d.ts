76212df3ef54dc5fc9f6e98371f706b813f96d68
layepythagore <layepythagore.91@gmail.com>
2019-05-09 14:57:07 +0000

    add method addclient

--- .sfdx/typings/lwc/apex/Lv_ClientCnt.d.ts
+++ .sfdx/typings/lwc/apex/Lv_ClientCnt.d.ts
@@ -1,3 +1,6 @@
 declare module "@salesforce/apex/Lv_ClientCnt.searchClient" {
   export default function searchClient(param: {numero: any, prenom: any, nom: any}): Promise<any>;
 }
+declare module "@salesforce/apex/Lv_ClientCnt.createClient" {
+  export default function createClient(param: {client: any}): Promise<any>;
+}
