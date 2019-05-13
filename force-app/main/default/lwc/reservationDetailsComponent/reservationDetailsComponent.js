339f9125235e80d9c0f30353c3c094fb77e85585
layepythagore <layepythagore.91@gmail.com>
2019-05-13 14:44:09 +0000

    fix bugs

--- force-app/main/default/lwc/reservationDetailsComponent/reservationDetailsComponent.js
+++ force-app/main/default/lwc/reservationDetailsComponent/reservationDetailsComponent.js
@@ -2,9 +2,21 @@ import { LightningElement, api, track } from 'lwc';
 
 export default class ReservationDetailsComponent extends LightningElement {
     @track reservation;
+    @track client;
+    @track vehicule;
 
     @api
     get reservation(){
         return this.reservation;
+    }
+
+    @api
+    get client(){
+        return this.client;
+    }
+
+    @api
+    get vehicule(){
+        return this.vehicule;
     }
 }
